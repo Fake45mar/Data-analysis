773373700e5a6e024d6f562e4a49e1cf2f124226
Lev <38050410+Fake45mar@users.noreply.github.com>
2019-07-17 10:04:25 +0300

    First version data analysis with matplotlib and numpy

---
+++ source.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    data_file = np.loadtxt('data_file.txt', delimiter=',')
+    time = data_file[:,0]
+    time = time - time[0]
+    sensors = data_file[:,1:3]
+    avg = np.mean(sensors, axis=1)
+    export = np.vstack((time, sensors.T))
+    export = export.T
+    np.savetxt('export.csv', export, delimiter=',')
+    plt.plot(time, sensors[:,1], 'ro')
+    plt.plot(time, avg, 'k.')
+    plt.show()
