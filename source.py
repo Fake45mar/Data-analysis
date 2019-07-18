2f0e97b6e403184a809fd700459892b640e0c77a
Lev <38050410+Fake45mar@users.noreply.github.com>
2019-07-18 19:31:36 +0300

    Added export to png

--- source.py
+++ source.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     avg = np.mean(sensors, axis=1)
     export = np.vstack((time, sensors.T))
     export = export.T
-    np.savetxt('export.csv', export, delimiter=',')
+    np.savetxt('export.png', export, delimiter=',')
     plt.plot(time, sensors[:,1], 'ro')
     plt.plot(time, avg, 'k.')
+    plt.savefig('foo.png', bbox_inches='tight')
     plt.show()
